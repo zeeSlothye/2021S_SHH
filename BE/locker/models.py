bccc46783d91deba9524117f23af49be5d59a0fa
sonjh <7dot83hz@gmail.com>
2021-08-27 23:25:28 +0900

    feat date => datetime

--- BE/locker/models.py
+++ BE/locker/models.py
@@ -21,8 +21,7 @@ class OccupiedLocker(models.Model):
     info = models.TextField(null = True,blank=True)
 
     isOpen = models.BooleanField(null = False, default=False)
-
-    date = models.DateField(null = False, default=timezone.now)#사용시작 시간
+    date = models.DateTimeField(null = False, default=timezone.now)#사용시작 시간
 
     trusterId = models.TextField(null = False)
 
