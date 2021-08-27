bc214913b24058442d7bfda9a735ac64e3675468
sonjh <7dot83hz@gmail.com>
2021-08-28 02:22:16 +0900

    feat create_occupiedLocker ispaid

--- BE/locker/views.py
+++ BE/locker/views.py
@@ -42,7 +42,6 @@ def create_occupiedLocker(request):
     # - trusterId (required)
     if(str(type(request.data)) == '<class \'django.http.request.QueryDict\'>'):
         #<class 'django.http.request.QueryDict'>
-        print(request)
         print(request.data.get('_content'))
         input = json.loads(request.data.get('_content')) 
         print(input)
