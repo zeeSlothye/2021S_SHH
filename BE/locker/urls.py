04b3fbac8802b3268f08af7014c7dd95f27ca76a
sonjh <7dot83hz@gmail.com>
2021-08-27 23:03:44 +0900

    refactor(occupiedLocker uri)

--- BE/locker/urls.py
+++ BE/locker/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('get_vacant_lockerS_stationIdx', get_vacant_lockerS_stationIdx),
-    path('create_occupiedLocker', create_occupiedLocker),
+    path('occupied/create', create_occupiedLocker),
     path('update_occupiedLocker', update_occupiedLocker)
 ]
